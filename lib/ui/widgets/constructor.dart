1fe216802dc8ffbc57628b8551346e4d598af918
ekrem-qb <ekrem.bayram@mail.ru>
2022-08-18 10:41:56 +0300

    Removed redundant custom stackboard items code Replaced constructor button icons

--- lib/ui/widgets/constructor.dart
+++ lib/ui/widgets/constructor.dart
@@ -1,49 +1,16 @@
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
 import 'package:stack_board/stack_board.dart';
 
 import '../../resources/images.dart';
 
-///自定义类型 Custom item type
-class CustomItem extends StackBoardItem {
-  const CustomItem({
-    required this.color,
-    Future<bool> Function()? onDelete,
-    int? id, // <==== must
-  }) : super(
-          child: const Text('CustomItem'),
-          onDelete: onDelete,
-          id: id, // <==== must
-        );
-
-  final Color? color;
-
-  @override // <==== must
-  CustomItem copyWith({
-    CaseStyle? caseStyle,
-    Widget? child,
-    int? id,
-    Future<bool> Function()? onDelete,
-    dynamic Function(bool)? onEdit,
-    bool? tapToEdit,
-    Color? color,
-  }) =>
-      CustomItem(
-        onDelete: onDelete,
-        id: id,
-        color: color ?? this.color,
-      );
-}
-
 class ConstructorScreen extends StatefulWidget {
   const ConstructorScreen({Key? key}) : super(key: key);
 
   @override
-  _ConstructorScreenState createState() => _ConstructorScreenState();
+  ConstructorScreenState createState() => ConstructorScreenState();
 }
 
-class _ConstructorScreenState extends State<ConstructorScreen> {
+class ConstructorScreenState extends State<ConstructorScreen> {
   late StackBoardController _boardController;
 
   @override
@@ -56,47 +23,6 @@ class _ConstructorScreenState extends State<ConstructorScreen> {
   void dispose() {
     _boardController.dispose();
     super.dispose();
-  }
-
-  /// 删除拦截
-  Future<bool> _onDel() async {
-    final bool? r = await showDialog<bool>(
-      context: context,
-      builder: (_) {
-        return Center(
-          child: SizedBox(
-            width: 400,
-            child: Material(
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    const Padding(
-                      padding: EdgeInsets.only(top: 10, bottom: 60),
-                      child: Text('确认删除?'),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        IconButton(
-                            onPressed: () => Navigator.pop(context, true),
-                            icon: const Icon(Icons.check)),
-                        IconButton(
-                            onPressed: () => Navigator.pop(context, false),
-                            icon: const Icon(Icons.clear)),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        );
-      },
-    );
-
-    return r ?? false;
   }
 
   @override
@@ -150,16 +76,15 @@ class _ConstructorScreenState extends State<ConstructorScreen> {
                   children: <Widget>[
                     const SizedBox(width: 25),
                     FloatingActionButton(
-                      onPressed: () {
-                        _boardController.add(
-                          const AdaptiveText(
-                            'Flutter Candies',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        );
-                      },
-                      child: const Icon(Icons.border_color),
-                    ),
+                        onPressed: () {
+                          _boardController.add(
+                            const AdaptiveText(
+                              'Flutter Candies',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          );
+                        },
+                        child: const Icon(Icons.text_fields_rounded)),
                     _spacer,
                     FloatingActionButton(
                       onPressed: () {
@@ -170,59 +95,29 @@ class _ConstructorScreenState extends State<ConstructorScreen> {
                           ),
                         );
                       },
-                      child: const Icon(Icons.image),
+                      child: const Icon(Icons.image_rounded),
                     ),
                     _spacer,
                     FloatingActionButton(
-                      onPressed: () {
-                        _boardController.add(
-                          const StackDrawing(
-                            caseStyle: CaseStyle(
-                              borderColor: Colors.grey,
-                              iconColor: Colors.white,
+                        onPressed: () {
+                          _boardController.add(
+                            const StackDrawing(
+                              caseStyle: CaseStyle(
+                                borderColor: Colors.grey,
+                                iconColor: Colors.white,
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                      child: const Icon(Icons.color_lens),
-                    ),
+                          );
+                        },
+                        child: const Icon(Icons.edit_rounded)),
                     _spacer,
-                    FloatingActionButton(
-                      onPressed: () {
-                        _boardController.add(
-                          StackBoardItem(
-                            child: const Text(
-                              'Custom Widget',
-                              style: TextStyle(color: Colors.black),
-                            ),
-                            onDelete: _onDel,
-                            // caseStyle: const CaseStyle(initOffset: Offset(100, 100)),
-                          ),
-                        );
-                      },
-                      child: const Icon(Icons.add_box),
-                    ),
-                    _spacer,
-                    FloatingActionButton(
-                      onPressed: () {
-                        _boardController.add<CustomItem>(
-                          CustomItem(
-                            color: Color((math.Random().nextDouble() * 0xFFFFFF)
-                                    .toInt())
-                                .withOpacity(1.0),
-                            onDelete: () async => true,
-                          ),
-                        );
-                      },
-                      child: const Icon(Icons.add),
-                    ),
                   ],
                 ),
               ),
             ),
             FloatingActionButton(
               onPressed: () => _boardController.clear(),
-              child: const Icon(Icons.close),
+              child: const Icon(Icons.delete_rounded),
             ),
           ],
         ),
@@ -231,29 +126,4 @@ class _ConstructorScreenState extends State<ConstructorScreen> {
   }
 
   Widget get _spacer => const SizedBox(width: 5);
-}
-
-class ItemCaseDemo extends StatefulWidget {
-  const ItemCaseDemo({Key? key}) : super(key: key);
-
-  @override
-  _ItemCaseDemoState createState() => _ItemCaseDemoState();
-}
-
-class _ItemCaseDemoState extends State<ItemCaseDemo> {
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        ItemCase(
-          isCentered: false,
-          child: const Text('Custom case'),
-          onDelete: () async {},
-          onOperationStateChanged: (OperationState operationState) => null,
-          onOffsetChanged: (Offset offset) => null,
-          onSizeChanged: (Size size) => null,
-        ),
-      ],
-    );
-  }
 }
